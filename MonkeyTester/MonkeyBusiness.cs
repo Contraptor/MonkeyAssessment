2ba2b319cc5354b913e1eff903aa8f9e4ca0e699
Contraptor <Contraptor@gmail.com>
2017-09-30 11:19:18 -0700

    Adding MonkeyBusiness.cs - file to hold Monkey and MonkeyManager classes. Also, some enumerations for CHASMSIDE, and POSITION

---
+++ MonkeyTester/MonkeyBusiness.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace MonkeyBusiness
+{
+
+    public enum CHASMSIDE {LEFT=0,RIGHT};
+    public enum POSITION { OFF = 0, ONE, TWO, THREE };
+
+
+
+    class Monkey
+    {
+        public CHASMSIDE Side;
+        public POSITION Spot;
+        public int CrossingCount;
+        public List<string> Errors;
+
+        private void clear()
+        {
+            this.Side = CHASMSIDE.LEFT;
+            this.Spot = POSITION.OFF;
+            this.CrossingCount = 0;
+            this.Errors = new List<string>();
+        }
+        public void Monkey(CHASMSIDE startingSide) 
+        {
+            this.clear();
+            this.Side = startingSide;
+        }
+
+        public POSITION PeekNextPosition()
+        {
+            switch (this.Side)
+            {
+                case CHASMSIDE.LEFT:
+
+                case CHASMSIDE.RIGHT:
+
+                default:
+                    Errors.Add("PeekNextPostion:Unexpected side of chasm");
+            }
+        }
+
+    }
+}
